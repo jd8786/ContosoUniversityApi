cec6275a5488e550d8eb4618d473b9db86508748
jason567893 <jason567893@gmail.com>
2018-07-13 17:06:41 -0400

    added student repo

---
+++ ContosoUniversity/Data/Repositories/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data.Repositories
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> GetStudents();
+
+        Student GetStudentById(int studentId);
+    }
+}
---
+++ ContosoUniversity/Data/Repositories/StudentRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data.Repositories
+{
+    public class StudentRepository: IStudentRepository
+    {
+        public readonly SchoolContext _context;
+
+        public StudentRepository(SchoolContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            var students = _context.Students.Include(s => s.Enrollments)
+                .ThenInclude(e => e.Course)
+                .ToList();
+
+            return students;
+        }
+
+        public Student GetStudentById(int studentId)
+        {
+            var student = GetStudents().FirstOrDefault(s => s.StudentId == studentId);
+
+            return student;
+        }
+    }
+}
