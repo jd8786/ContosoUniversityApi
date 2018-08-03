25ba6e005fda34a56a4999fc2c7868badba1e2e0
jason567893 <jason567893@gmail.com>
2018-08-03 17:11:44 -0400

    added swagger

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ContosoUniversity
 {
@@ -29,6 +30,18 @@ namespace ContosoUniversity
             services.AddDbContext<SchoolContext>(options => options.UseSqlServer(schoolDbConnectionString));
 
             services.AddScoped<IStudentsRepository, StudentsRepository>();
+
+            services.AddSwaggerGen(o =>
+            {
+                o.SwaggerDoc("v1", new Info
+                {
+                    Title = "Contoso University",
+                    Version = "v1",
+                    Description = "Services for Contoso University application"
+                });
+
+                o.EnableAnnotations();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +54,14 @@ namespace ContosoUniversity
 
             app.UseCors(builder =>
                 builder.WithOrigins("http://localhost:4200"));
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Students");
+                c.DocumentTitle = "Contoso University API Docs";
+            });
 
             app.UseMvc();
         }
