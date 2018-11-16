2d5c7cd7960413cbe21b3b88304bc2b4e57628b7
jason567893 <jason567893@gmail.com>
2018-11-16 16:18:00 -0500

    added acceptance test for student by id

---
+++ ContosoUniversity.Api.Acceptance.Test/Controllers/Students/GetStudentByIdTests.cs
@@ -0,0 +1,36 @@
+﻿using ContosoUniversity.Api.Models;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using Xunit;
+
+namespace ContosoUniversity.Api.Acceptance.Test.Controllers.Students
+{
+    public class GetStudentByIdTests: IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public GetStudentByIdTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async void ShouldReturnOkWhenRetrievingStudentById()
+        {
+            var apiResponse = await _client.GetAsync("api/students/3");
+
+            apiResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await apiResponse.Content.ReadAsStringAsync();
+
+            var responseOfStudent = JsonConvert.DeserializeObject<ApiResponse<Student>>(content);
+
+            responseOfStudent.Data.StudentId.Should().Be(3);
+            responseOfStudent.Data.LastName.Should().Be("Anand");
+            responseOfStudent.Data.FirstMidName.Should().Be("Arturo");
+            responseOfStudent.Data.OriginCountry.Should().Be("TURKEY");
+        }
+    }
+}
