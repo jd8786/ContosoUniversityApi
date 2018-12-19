e3d362fbf4caf55b5c5d51c5aec0e9ddaa3c3a40
jason567893 <jason567893@gmail.com>
2018-12-19 16:43:27 -0500

    added unit tests for delete student endpoint

---
+++ ContosoUniversity.Api.Test/Controllers/Students/DeleteStudentTests.cs
@@ -0,0 +1,81 @@
+﻿using ContosoUniversity.Api.Controllers;
+using ContosoUniversity.Api.Models;
+using ContosoUniversity.Api.Services;
+using ContosoUniversity.Data.Exceptions;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ContosoUniversity.Api.Test.Controllers.Students
+{
+    [Trait("Category", "Unit Test: Api.Controllers.Students.DeleteStudent")]
+    public class DeleteStudentTests
+    {
+        private readonly Mock<IStudentsService> _studentService;
+
+        private readonly StudentsController _controller;
+
+        public DeleteStudentTests()
+        {
+            _studentService = new Mock<IStudentsService>();
+            _controller = new StudentsController(_studentService.Object, null);
+        }
+
+        [Fact]
+        public void ShouldReturnOkResponse()
+        {
+            var response = _controller.DeleteStudent(1);
+
+            _studentService.Verify(s => s.Remove(1), Times.Exactly(1));
+
+            var okResponse = (OkObjectResult)response;
+
+            okResponse.StatusCode.Should().Be(200);
+
+            var responseObject = (ApiResponse<bool>)okResponse.Value;
+
+            responseObject.IsSuccess.Should().BeTrue();
+
+            responseObject.Data.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldReturnNotFoundWhenThrowingNotFoundException()
+        {
+            _studentService.Setup(s => s.Remove(It.IsAny<int>())).Throws(new NotFoundException("some-error-message"));
+
+            var response = _controller.DeleteStudent(It.IsAny<int>());
+
+            var errorResponse = (ObjectResult)response;
+
+            errorResponse.StatusCode.Should().Be(404);
+
+            var responseObject = (ApiResponse<bool>)errorResponse.Value;
+
+            responseObject.IsSuccess.Should().BeFalse();
+
+            responseObject.Messages.Should().BeEquivalentTo(new List<string> { "some-error-message" });
+        }
+
+        [Fact]
+        public void ShouldReturnInternalServerErrorWhenThrowingException()
+        {
+            _studentService.Setup(s => s.Remove(It.IsAny<int>())).Throws(new Exception("some-error-message"));
+
+            var response = _controller.DeleteStudent(It.IsAny<int>());
+
+            var errorResponse = (ObjectResult)response;
+
+            errorResponse.StatusCode.Should().Be(500);
+
+            var responseObject = (ApiResponse<bool>)errorResponse.Value;
+
+            responseObject.IsSuccess.Should().BeFalse();
+
+            responseObject.Messages.Should().BeEquivalentTo(new List<string>{"some-error-message"});
+        }
+    }
+}
