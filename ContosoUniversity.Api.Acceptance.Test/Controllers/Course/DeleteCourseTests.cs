3b9027627aff4a8003b400822db137d092fd74b7
jason567893 <jason567893@gmail.com>
2019-02-13 17:11:56 -0500

    added update and delete acceptance tests for course

---
+++ ContosoUniversity.Api.Acceptance.Test/Controllers/Course/DeleteCourseTests.cs
@@ -0,0 +1,43 @@
+﻿using ContosoUniversity.Api.Acceptance.Test.Fixtures;
+using ContosoUniversity.Api.Models;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+using Xunit;
+
+namespace ContosoUniversity.Api.Acceptance.Test.Controllers.Course
+{
+    [Collection("Sequential")]
+    [Trait("Category", "Acceptance Test: Api.Controllers.Course")]
+    public class DeleteCourseTests
+    {
+        private readonly AcceptanceTestFixture _fixture;
+
+        public DeleteCourseTests(AcceptanceTestFixture fixture)
+        {
+            _fixture = fixture;
+            _fixture.ResetDatabase();
+        }
+
+        [Fact]
+        public async void ShouldReturnOkWhenDeletingCourse()
+        {
+            var course = _fixture.SchoolContext.Courses.First(c => c.Title == "test-title1");
+
+            var courseId = course.CourseId;
+
+            var apiResponse = await _fixture.HttpClient.DeleteAsync($"api/courses/{courseId}");
+
+            apiResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await apiResponse.Content.ReadAsStringAsync();
+
+            var response = JsonConvert.DeserializeObject<ApiResponse<bool>>(content);
+
+            response.Data.Should().BeTrue();
+
+            _fixture.SchoolContext.Courses.ToList().Any(c => c.Title == "test-title1").Should().BeFalse();
+        }
+    }
+}
---
+++ ContosoUniversity.Api.Acceptance.Test/Controllers/Course/UpdateCourseTests.cs
@@ -0,0 +1,84 @@
+﻿using AutoFixture;
+using ContosoUniversity.Api.Acceptance.Test.Fixtures;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+using ApiModels = ContosoUniversity.Api.Models;
+
+namespace ContosoUniversity.Api.Acceptance.Test.Controllers.Course
+{
+    [Collection("Sequential")]
+    [Trait("Category", "Acceptance Test: Api.Controllers.Course")]
+    public class UpdateCourseTests
+    {
+        private readonly AcceptanceTestFixture _fixture;
+
+        private readonly Fixture _autoFixture;
+
+        public UpdateCourseTests(AcceptanceTestFixture fixture)
+        {
+            _fixture = fixture;
+            _fixture.ResetDatabase();
+            _autoFixture = new Fixture();
+        }
+
+        [Fact]
+        public async void ShouldReturnOkWhenUpdatingCourse()
+        {
+            var existingCourse = _fixture.SchoolContext.Courses.First(c => c.Title == "test-title1");
+
+            var existingStudents = _fixture.SchoolContext.Students.ToList();
+
+            var students = new List<ApiModels.Student>
+            {
+                new ApiModels.Student {StudentId = existingStudents[0].StudentId},
+                new ApiModels.Student {StudentId = existingStudents[1].StudentId}
+            };
+
+            var existingInstructors = _fixture.SchoolContext.Instructors.ToList();
+
+            var instructors = new List<ApiModels.Instructor>
+            {
+                new ApiModels.Instructor {InstructorId = existingInstructors[0].InstructorId},
+                new ApiModels.Instructor {InstructorId = existingInstructors[1].InstructorId}
+            };
+
+            var existingDepartments = _fixture.SchoolContext.Departments.ToList();
+
+            var department = existingDepartments.First(d => d.Name == "test-name2");
+
+            var courseId = existingCourse.CourseId;
+
+            var course = _autoFixture.Build<ApiModels.Course>()
+                .With(c => c.CourseId, courseId)
+                .With(c => c.Title, "some-title")
+                .With(c => c.Credits, 10)
+                .With(c => c.Department, new ApiModels.Department { DepartmentId = department.DepartmentId})
+                .With(c => c.Students, students)
+                .With(c => c.Instructors, instructors)
+                .Create();
+
+            var studentJson = JsonConvert.SerializeObject(course);
+
+            var content = new StringContent(studentJson, Encoding.UTF8, "application/json");
+
+            var apiResponse = await _fixture.HttpClient.PutAsync("api/courses", content);
+
+            apiResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var dbCourse = _fixture.SchoolContext.Courses.Include(c => c.Enrollments).Include(c => c.CourseAssignments).Include(c => c.Department).First(c => c.CourseId == courseId);
+
+            dbCourse.Title.Should().Be("some-title");
+            dbCourse.Credits.Should().Be(10);
+            dbCourse.Department.Name.Should().Be("test-name2");
+            dbCourse.Enrollments.Count.Should().Be(2);
+            dbCourse.CourseAssignments.Count.Should().Be(2);
+        }
+    }
+}
